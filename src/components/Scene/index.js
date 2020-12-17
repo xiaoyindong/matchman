f80561712c060bb0b22a58d22a3e8474b8b97af9
yindong <812748570@qq.com>
2020-12-17 22:10:53 +0800

    feat: 初始化

--- src/components/Scene/index.js
+++ src/components/Scene/index.js
@@ -5,6 +5,11 @@ class Scene {
         this.bgi = bgi;
         this.container = container;
         this.render();
+        this.translateX = 0;
+        this.translateY = 0;
+        this.speedX = 0;
+        this.speedY = 0;
+        this.moving = false;
     }
     render() {
         this.ele = document.createElement('div');
@@ -12,6 +17,37 @@ class Scene {
         this.ele.style.backgroundImage = `url(${this.bgi})`;
         this.ele.className = 'background_part';
         this.container.append(this.ele);
+    }
+
+    move(directs) {
+        this.speedX = 0;
+        this.speedY = 0;
+        if (directs.includes('right')) {
+            this.speedX = -3;
+        } else if (directs.includes('left')) {
+            this.speedX = 3;
+        }
+
+        if (directs.includes('down')) {
+            this.speedY = -5;
+        } else if (directs.includes('up')) {
+            this.speedY = 5;
+        }
+        if (directs.length > 0) {
+            this.animation();
+            this.moving = true;
+        } else {
+            this.moving = false;
+        }
+    }
+
+    animation() {
+        if (this.speedX || this.speedY) {
+            this.translateX += this.speedX;
+            this.translateY += this.speedY;
+            this.ele.style.transform = `translate(${this.translateX}px, ${this.translateY}px)`;
+            requestAnimationFrame(this.animation.bind(this));
+        }
     }
     
     getEle() {
--- src/main.js
+++ src/main.js
@@ -6,11 +6,42 @@ import Person from './components/Person';
 import Scene from './components/Scene';
 import Games from './components/Games';
 
+const direct = {
+    87: 'up',
+    65: 'left',
+    83: 'down',
+    68: 'right',
+    74: 'j',
+    75: 'k',
+    76: 'l',
+    13: 'enter',
+    27: 'esc'
+}
+
+const keys = [];
+
 // 初始化窗口
 const App = new Games();
 const client = App.getEle();
 // 初始化场景
-new Scene(bgi, client);
+const scene = new Scene(bgi, client);
 // new Person('隐冬', 1);
+
+window.addEventListener('keydown', (e) => {
+    const key = direct[e.keyCode];
+    if (!keys.includes(key) && key) {
+        keys.push(key);
+        scene.move(keys);
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    const key = direct[e.keyCode];
+    const idx = keys.indexOf(key);
+    if (idx >= 0 && key) {
+        keys.splice(idx, 1);
+        scene.move(keys);
+    }
+})
 
 
